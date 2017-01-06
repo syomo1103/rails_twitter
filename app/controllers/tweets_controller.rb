12e275e23d1343d6b17f226572cccaba679ecc5a
Stephanie Yomorta <stephanie.yomorta@gmail.com>
2017-01-06 10:40:24 -0800

    added routes

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,25 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
