7ef81c86b35cacd8dc59853b29013af31fe34acf
Stephanie Yomorta <stephanie.yomorta@gmail.com>
2017-01-09 10:17:59 -0800

    updates to styling

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,21 +5,40 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.new(params.require(:tweet).permit(:handle, :content))
+      if @tweet.save
+        redirect_to tweets_path
+      else
+        render :new
+      end
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
+    @tweet = Tweet.find(params[:id])
+
+    if @tweet.update_attributes(params.require(:tweet).permit(:handle, :content))
+      redirect_to tweets_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
   end
 
 end
